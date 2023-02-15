5d4ad88c54a10ecab33db7af446a3e0bdfc5f492
Trudy <gcyn51a@gmail.com>
2023-02-15 11:36:22 +0300

    Create a controller to handle file uploads.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,5 +8,7 @@ const router = express.Router();
 // Routes
 router.route("/users/new").post(apiController.createUser);
 router.route("/users").get(apiController.getAllUsers);
+router.route('/photos/upload').post(apiController.uploadFile)
+router.route('/bucket/list').get(apiController.getFiles)
 
 module.exports = router;
